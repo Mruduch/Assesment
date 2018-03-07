a66a49207fd5494142a7a9c3b991e03ed7d9a84b
Mruduch <mruduch86@gmail.com>
2018-03-07 18:36:24 +0000

    Uploading All the files from Assesment project

---
+++ Selenium/src/findCompatibility.java
@@ -0,0 +1,23 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class findCompatibility {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Home\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.sword-activerisk.com/");
+
+		 WebElement element = driver.findElement(By.name("s"));
+         element.sendKeys("Compatibility");
+         element.submit();
+
+	}
+
+}
